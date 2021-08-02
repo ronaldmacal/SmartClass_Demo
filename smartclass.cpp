1361fd9c1f13094fb9e4727a8bd10f057ff408dd
Ronald Macal <bark_ronald@hotmail.com>
2021-08-01 22:56:23 -0600

    Inicio

---
+++ smartclass.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+void menuprincipal();
+int main (){
+    menuprincipal();
+    return 0;
+
+}
+
+void menuprincipal(){
+    cout<<"\t\t\t Elija una opcion\n\n";
+    cout<<"1. Carga de usuarios\n";
+    cout<<"2. Carga de tareas\n";
+    return;
+}
